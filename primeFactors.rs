4f7d13a296a1593fc24a8a024e949a4e8fea39cb
another-dave <whatsyourfavouritehummingnoise+github@gmail.com>
2014-01-07 09:06:21 +0000

    Need to refactor for general case; have to move away from match/case I think, but will check in (failing) WIP to move between computers

--- primeFactors.rs
+++ primeFactors.rs
@@ -1,61 +1,82 @@
-fn primeFactors(n: int) -> @[int] {
-	match n {
-		0 => @[],
-		1..3 => @[n],
-		4..10 => {
+fn primeFactors(n: int, currentResult: @[int]) -> @[int] {
+	currentResult + match n {
+		0 | 1 => @[],
+		2..3 => @[n],
+		_ => {
 			if n % 2 == 0 {
-				@[2,n/2]
+				primeFactors(2, primeFactors(n/2, @[]))
+			} else if n % 3 == 0 {
+				primeFactors(3, primeFactors(n/3, @[]))	
 			} else {
 				@[n]
 			}
-		},
-		_ => @[-1]
+		}
 	}
 }
 
-#[test]
-fn that_there_are_no_prime_factors_of_zero() {
-	let zeroFactors = primeFactors(0);
-	assert!(zeroFactors.is_empty(), "Zero should have no prime factors")
+fn assertResult(numberToCheck: int, expectedResult: @[int]) {
+	let actualResult = primeFactors(numberToCheck, @[]);
+	assert!(actualResult == expectedResult, 
+		"The prime factor of \""+ numberToCheck.to_str() + "\" should be \"" + expectedResult.to_str() 
+			+ "\", but was \"" + actualResult.to_str() + "\"");
 }
 
 #[test]
-fn that_the_prime_factor_of_one_is_one() {
-	assert!(primeFactors(1) == @[1], "The prime factor of one should be '1'")
+fn zero() {
+	assertResult(0, @[])
 }
 
 #[test]
-fn that_the_prime_factor_of_two_is_two() {
-	assert!(primeFactors(2) == @[2], "Prime factors of '2' should be '2'")
+fn one() {	
+	assertResult(1, @[])
 }
 
 #[test]
-fn that_the_prime_factor_of_three_is_three() {
-	assert!(primeFactors(3) == @[3], "Prime factors of '3' should be '3'")
+fn two() {
+	assertResult(2, @[2])
 }
 
 #[test]
-fn that_the_prime_factors_of_four_is_two_and_two() {
-	assert!(primeFactors(4) == @[2, 2], "Prime factors of 4 should be '2, 2'")
+fn three() {
+	assertResult(3, @[3])
 }
 
 #[test]
-fn that_the_prime_factor_of_five_is_five() {
-	assert!(primeFactors(5) == @[5], "Prime factor of 5 should be '5'")
+fn four() {
+	assertResult(4, @[2,2])
 }
 
 #[test]
-fn that_the_prime_factors_of_six_is_three_and_two() {
-	let factors = primeFactors(6);
-	assert!(factors == @[2, 3], "Prime factors of 6 should be '2, 3'")
+fn five() {
+	assertResult(5, @[5])
+}
+
+#[test]
+fn six(){
+	assertResult(6, @[3, 2])
 }
 
 #[test]
 fn seven() {
-	assert!(primeFactors(7) == @[7], "Result for seven should be \"7\"")
+	assertResult(7, @[7])
 }
 
 #[test]
 fn eight() {
-	assert!(primeFactors(8) == @[2,2,2,2], "Result for eight should be \"2,2,2,2\"")
+	assertResult(8, @[2,2,2])
+}
+
+#[test]
+fn nine() {
+	assertResult(9, @[3,3])
+}
+
+#[test]
+fn ten() {
+	assertResult(10, @[5, 2])
+}
+
+#[test]
+fn ninetyOne() {
+	assertResult(91, @[13, 7])
 }
